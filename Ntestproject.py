c035b5739a3acfcf5548bf9a43b901aa4e9d7174
Suited-Mystery <raffanoeramadhan@gmail.com>
2024-07-12 18:18:18 +0700

    Add files via upload
    just some small projects of mine, feel free to suggest me some projects! (in my skill level ofc)

---
+++ Ntestproject.py
@@ -0,0 +1,21 @@
+import time
+
+print("---------------------------------------")
+print("Welcome to your first Pyhon Program! Just answer some questions first") 
+
+time.sleep(3)
+
+name = input("What's your name?: ")
+time.sleep(1)
+print("That's a very nice name!")
+
+time.sleep(2)
+
+age = int(input("And how old are you?: "))
+time.sleep(1)
+print("Amazing!")
+
+time.sleep(3)
+
+print("Hello,", name,"! And you're", age, "right? Nice to meet you!")
+print("---------------------------------------")
---
+++ Pythoncalculator.py
@@ -0,0 +1,78 @@
+history = []
+
+def addition():
+    print("Please enter 2 numbers to add:")
+    a = int(input("a = "))
+    b = int(input("b = "))
+    addresult = a + b
+    result = ("Result of " +str(a)+ " + " +str(b)+ " = " +str(addresult))
+    print(result)
+    history.append(result)
+
+def subtraction():
+    print("Please enter 2 numbers to subtract:")
+    a = int(input("a = "))
+    b = int(input("b = "))
+    subtractresult = a - b
+    result = ("Result of " +str(a)+ " - " +str(b)+ " = " +str(subtractresult))
+    print(result)
+    history.append(result)
+
+def multiplication():
+    print("Please enter 2 numbers to multiply:")
+    a = int(input("a = "))
+    b = int(input("b = "))
+    multiplyresult = a * b
+    result = ("Result of " +str(a)+ " * " +str(b)+ " = " +str(multiplyresult))
+    print(result)
+    history.append(result)
+    
+def division():
+    print("Please enter 2 numbers to divide:")
+    a = int(input("a = "))
+    b = int(input("b = "))
+    divideresult = a / b
+    result = ("Result of " +str(a)+ " / " +str(b)+ " = " +str(divideresult))
+    print(result)
+    history.append(result)
+
+def exponentiation():
+    print("Please enter the first number to be raised to the power of the second number:")
+    a = int(input("a = "))
+    b = int(input("b = "))
+    exponentresult = a ** b
+    result = ("Result of " +str(a)+ " ** " +str(b)+ " = " +str(exponentresult))
+    print(result)
+    history.append(result)
+
+def main():
+    while True:
+        print("\nChoose an option:")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exponentiation")
+        print("6. View History")
+        print("7. Quit")
+        choice = input("Choice: ")
+    
+        if choice == "1":
+            addition()
+        elif choice == "2":
+            subtraction()
+        elif choice == "3":
+            multiplication()
+        elif choice == "4":
+            division()
+        elif choice == "5":
+            exponentiation()
+        elif choice == "6":
+            print(history)
+        elif choice == "7":
+            break
+        else:
+            print("Please enter a valid choice.")
+
+if __name__ == "__main__":
+    main()
